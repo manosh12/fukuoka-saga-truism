d9f854aba9edb0402283e1f68dcad4a0c370f0a9
manosh <pun@thc.jpn.com>
2024-06-14 17:04:29 +0900

    add i18n for multi language

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+// src/components/Header.jsx
+import React, { useState } from "react";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem } from "@nextui-org/react";
 import { Link } from "react-router-dom";
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
+import { FaGlobe } from 'react-icons/fa';
+import { useTranslation } from 'react-i18next';
 import '../App.css';
 import { TopScroll } from "./TopScroll.jsx";
+import i18n from '../i18n';
 
 export const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { t } = useTranslation();
 
   const menuItems = [
-    { name: "HOME", link: "/" },
-    { name: "福岡観光", link: "/#fukuokaTruism" },
-    { name: "佐賀観光", link: "/#sagaTruism" },
-    { name: "お問い合わせ", link: "/#contact" },
+    { name: t('Navbar.home'), link: "/" },
+    { name: t('Navbar.confirmation'), link: "/#fukuokaTruism" },
+    { name: t('Navbar.payment'), link: "/#sagaTruism" },
+    { name: t('Navbar.complete'), link: "/#contact" },
   ];
 
   const handleMenuClose = () => {
     setIsMenuOpen(false);
+  };
+
+  const handleLanguageChange = (lang) => {
+    i18n.changeLanguage(lang);
   };
 
   return (
@@ -25,11 +35,11 @@ export const Header = () => {
           <Link to="/" onClick={TopScroll}>
             <div className="flex justify-start items-start gap-2">
               <img src="/images/logo.png" alt="logo" className="w-10 h-auto" />
-              <h4 className="font-bold text-lg mt-2">福岡・佐賀観光</h4>
+              <h4 className="font-bold text-lg mt-2">{t('Navbar.home')}</h4>
             </div>
           </Link>
         </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-10 justify-end">
+        <NavbarContent className="hidden sm:flex gap-10 justify-end items-center">
           {menuItems.map((item, index) => (
             <NavbarItem key={index} isActive={index === 0}>
               <Link
@@ -41,12 +51,43 @@ export const Header = () => {
               </Link>
             </NavbarItem>
           ))}
+          <Dropdown>
+            <DropdownTrigger>
+              <Button
+                variant="bordered"
+                className="flex items-center text-sm border-1"
+                aria-label="Select language"
+                radius="sm"
+              >
+                <FaGlobe className="" size={20} />
+                {i18n.language === "en" ? "English" : "日本語"}
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu
+              aria-label="Language selection"
+              onAction={(key) => handleLanguageChange(key)}
+              className="dropdown-menu"
+            >
+              <DropdownItem key="en">
+                <div className="flex items-center">
+                  English
+                </div>
+              </DropdownItem>
+              <DropdownItem key="jp">
+                <div className="flex items-center">
+                  日本語
+                </div>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </NavbarContent>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           className="ml-auto sm:hidden"
         />
       </NavbarContent>
+
+      {/*mobile*/}
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={index} className="mobile-menu-item">
@@ -62,7 +103,39 @@ export const Header = () => {
             </Link>
           </NavbarMenuItem>
         ))}
+        <NavbarMenuItem className="mobile-menu-item">
+          <Dropdown>
+            <DropdownTrigger>
+              <Button
+                variant="bordered"
+                className="flex items-center w-full"
+                aria-label="Select language"
+              >
+                <FaGlobe className="mr-2" size={20} />
+                {i18n.language === "en" ? "English" : "日本語"}
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu
+              aria-label="Language selection"
+              onAction={(key) => handleLanguageChange(key)}
+              className="dropdown-menu"
+            >
+              <DropdownItem key="en">
+                <div className="flex items-center">
+                  <FaGlobe className="mr-2" size={20} />
+                  English
+                </div>
+              </DropdownItem>
+              <DropdownItem key="jp">
+                <div className="flex items-center">
+                  <FaGlobe className="mr-2" size={20} />
+                  日本語
+                </div>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </NavbarMenuItem>
       </NavbarMenu>
     </Navbar>
   );
-}
+};
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {NextUIProvider} from '@nextui-org/react'
+import { NextUIProvider } from '@nextui-org/react'
+import "i18n";
+import { I18nextProvider } from 'react-i18next';
+import i18next from "i18next"; // Ensure this path is correct
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
-  </React.StrictMode>,
+      <NextUIProvider>
+
+        <App />
+
+      </NextUIProvider>
+  </React.StrictMode>
 )
